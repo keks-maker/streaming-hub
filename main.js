886a2f60665066bab4ea639d1bee4ed8cd2af6e5
keks <keks@streaming-hub>
2026-05-18 15:46:45 +0200

    v0.2.0 – main.js Version History korrigiert (war v0.2.1)

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow, ipcMain, components, screen } = require('electron');
-const path = require('path');
-
 const fs = require('fs');
+const path = require('path');
 
 let mainWindow;
 let pipWindow = null;
