4268e0c6d3e6d20e0612c82807b48c5487ff9be8
keks <keks@streaming-hub>
2026-05-18 15:12:09 +0200

    v0.2.2 – Google-Login: Chrome-Objekt + webdriver via executeJavaScript

--- preload-content.js
+++ preload-content.js
@@ -1,11 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-Object.defineProperty(Object.getPrototypeOf(navigator), 'webdriver', {
-  value: undefined,
-  writable: false,
-  configurable: true,
-});
-
 contextBridge.exposeInMainWorld('electronAPI', {
   minimizeWindow: () => ipcRenderer.send('minimize-window'),
   closeWindow: () => ipcRenderer.send('close-window'),
