75005386967e65d4f47a896043c0e8511c3faf7d
keks <keks@streaming-hub>
2026-05-18 15:24:49 +0200

    v0.1.14 – Cache-Bereinigung aus start.sh entfernt (Login persistiert nun)

--- start.sh
+++ start.sh
@@ -7,16 +7,7 @@ APP_DIR="$(dirname "$(readlink -f "$0")")"
 export PATH="$HOME/.local/node/bin:$HOME/.local/bin:$PATH"
 export ELECTRON_DISABLE_SANDBOX=1
 
-# Clear stale cookies and caches (Google-Login-Fix)
-PART_DIR="$HOME/.config/streaming-hub/Partitions/streaming"
-if [ -d "$PART_DIR" ]; then
-  rm -rf "$PART_DIR/Cookies" "$PART_DIR/Cookies-journal" \
-         "$PART_DIR/Local Storage" "$PART_DIR/Session Storage" \
-         "$PART_DIR/Service Worker" "$PART_DIR/Cache" \
-         "$PART_DIR/Code Cache" "$PART_DIR/blob_storage" \
-         "$PART_DIR/Shared Dictionary" "$PART_DIR/IndexedDB" \
-         "$PART_DIR/SharedStorage" "$PART_DIR/DIPS" 2>/dev/null
-fi
+
 
 cd "$APP_DIR"
 
