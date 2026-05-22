115eb564b7241ee96f8fac98f14f281e13bce368
keks <keks@streaming-hub>
2026-05-22 22:29:23 +0200

    fix: Symlink-Auflösung in start.sh (readlink -f)

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Streaming Hub - Castlabs Electron mit Widevine DRM Support
 
-APP_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+APP_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd -P)"
 
 export ELECTRON_DISABLE_SANDBOX=1
 
